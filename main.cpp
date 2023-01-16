12ac7c54765aba116c8dc57e5d3b0cef3e848d9f
Kyle Houston <kdh29@calvin.edu>
2023-01-15 23:17:47 -0500

    proj0 done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-
+#include <string>
+#include <iostream>
+using namespace std;
 int main() {
-
+    float a_ray[20] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,1.0,2.0};
+    cout <<  sizeof(a_ray) << endl;
     return 0;
 }
